bde96925bfb453a25e90725d3a98513d3433765e
DaNiLo <danilo.p2391@gmail.com>
2025-08-27 18:17:17 +0200

    First commit

---
+++ movie_storage_sql.py
@@ -0,0 +1,64 @@
+""" Create the engine and connect to the database """
+from sqlalchemy import create_engine, text
+
+# Define the database URL
+DB_URL = "sqlite:///movies.db"
+# Create the engine to connect to the DB
+engine = create_engine(DB_URL, echo=False)
+
+
+def list_movies():
+    """Retrieve all movies from the database."""
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT title, year, rating FROM movies"))
+        movies = result.fetchall()
+
+    return {row[0]: {"year": row[1], "rating": row[2]} for row in movies}
+
+
+def add_movie(title, year, rating, img_url):
+    """Add a new movie to the database."""
+    with engine.connect() as connection:
+        try:
+            connection.execute(text("INSERT INTO movies (title, year, rating, img_url) VALUES (:title, :year, :rating, :img_url)"),
+                               {"title": title, "year": year, "rating": rating, "img_url": img_url})
+            connection.commit()
+            print(f"Movie '{title}' added successfully.")
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+def delete_movie(title):
+    """Delete a movie from the database."""
+    with engine.connect() as connection:
+        try:
+            connection.execute(
+                text("DELETE FROM movies WHERE title = :title"),
+                {"title": title})
+            connection.commit()
+            print(f"Movie '{title}' deleted successfully.")
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+def update_movie(title, rating):
+    """Update a movie's rating in the database."""
+    pass
+
+
+def create_movies_table():
+    """Create the movies table if it does not exist"""
+    with engine.connect() as connection:
+        connection.execute(text("""
+            CREATE TABLE IF NOT EXISTS movies (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT UNIQUE NOT NULL,
+                year INTEGER NOT NULL,
+                rating REAL NOT NULL,
+                img_url TEXT NOT NULL
+            )
+        """))
+        connection.commit()
+
+
+create_movies_table()
---
+++ movies.py
@@ -0,0 +1,272 @@
+"""Run commands to manage a movie database."""
+import os
+
+import requests
+import random
+import statistics
+import shutil
+import movie_storage_sql as storage
+
+API_KEY = "8da11d34"
+API_URL = f"http://www.omdbapi.com/?i=tt3896198&apikey={API_KEY}&"
+
+class BColors:
+    """COLORS AND TEXT FORMATTING FOR THE CLI CONTENTS"""
+    HEADER = '\033[95m'
+    OK_BLUE = '\033[94m'
+    OK_CYAN = '\033[96m'
+    OK_GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+CLI_MAIN_MENU = f'''
+{BColors.OK_CYAN}{BColors.UNDERLINE}{BColors.BOLD}{'〰️' * 5} DB from Danilo {'〰️' * 5}{BColors.ENDC}
+{BColors.BOLD}{BColors.HEADER}#. Menu options{BColors.ENDC}
+{BColors.OK_BLUE}0. Quit{BColors.ENDC}
+{BColors.OK_BLUE}1. List movies{BColors.ENDC}
+{BColors.OK_BLUE}2. Add movie{BColors.ENDC}
+{BColors.OK_BLUE}3. Delete movie{BColors.ENDC}
+{BColors.OK_BLUE}4. Update movie{BColors.ENDC}
+{BColors.OK_BLUE}5. Stats{BColors.ENDC}
+{BColors.OK_BLUE}6. Random movie{BColors.ENDC}
+{BColors.OK_BLUE}7. Search movie{BColors.ENDC}
+{BColors.OK_BLUE}8. Movies sorted by rating{BColors.ENDC}
+{BColors.OK_BLUE}9. Generate Website{BColors.ENDC}
+{'〰️' * 18}\n
+{BColors.HEADER}{BColors.BOLD}Enter choice (0-8): {BColors.ENDC}'''
+
+
+def command_list_movies():
+    """Retrieve and display all movies from the database."""
+    movies = storage.list_movies()
+    print(f"\n{BColors.OK_CYAN}#️⃣ {len(movies)} movies in total{BColors.ENDC}")
+    for movie, data in movies.items():
+        print(f"{BColors.OK_BLUE}🎬 {movie}: {data['rating']} ({data['year']}){BColors.ENDC}")
+
+
+def command_add_movie():
+    """Add a new movie to the database."""
+    while True:
+        movies =  storage.list_movies()
+        title = input('\nChoose the movie title: ')
+        if title in movies:
+            print(f"{BColors.FAIL}{BColors.BOLD}Movie {title} already exist!{BColors.ENDC}")
+            continue
+
+        if title == '':
+            print(f"{BColors.FAIL}{BColors.BOLD}You need to type something!{BColors.ENDC}")
+            continue
+
+        params = {
+            "type": "movie",
+            't': title,
+        }
+        # Fetch movies data from OMDbAPI
+        try:
+            api_result = requests.get(API_URL, params=params).json()
+        except requests.exceptions.RequestException as error:
+            print(f"{BColors.FAIL}Can't get a valid response: "
+            f"{str(error).split('(')[0]}{BColors.ENDC}")
+            continue
+
+        if api_result['Response'] == 'False' or len(api_result) == 0:
+            print(f"{BColors.FAIL}{BColors.BOLD}Movie "
+            f"{title} not found!{BColors.ENDC}")
+            continue
+
+        # Check if all datas are valid
+        year = None
+        rating = None
+        img_url = None
+
+        if len(api_result['Year']) == 4:
+            year = api_result['Year']
+
+        if len(api_result['Ratings']) > 0:
+            rating = api_result['Ratings'][0]['Value'].split('/')[0]
+
+        if len(api_result['Poster']) > 0:
+            img_url = api_result['Poster']
+
+        # stops if any of the required data is missing
+        if year is None or rating is None or img_url is None:
+            print(f"{BColors.FAIL}{BColors.BOLD}Movie "
+                  f"{title} has missing data!{BColors.ENDC}")
+            continue
+
+        # add fetched data to the database
+        storage.add_movie(title, year, rating, img_url)
+        print(f"\n{BColors.OK_GREEN}{BColors.BOLD}{title} ({year}) "
+              f"is now in the DB with the rating of {rating}{BColors.ENDC}")
+        break
+
+
+def command_delete_movie():
+    """Delete a movie from the database."""
+    while True:
+        movies =  storage.list_movies()
+        title = input('\nSelect movie to delete: ')
+        if title not in movies:
+            print(f'\n{BColors.FAIL}{BColors.BOLD}Movie is not in the DB{BColors.ENDC}')
+            continue
+
+        storage.delete_movie(title)
+        print(f'{BColors.OK_GREEN}{BColors.BOLD}{title} deleted!{BColors.ENDC}')
+        break
+
+
+def update_movie():
+    """UPDATE RATING OF A MOVIE"""
+    movies = storage.list_movies()
+    while True:
+        title = input('\nType movie title to update: ')
+
+        if title not in movies:
+            print(f'\n{BColors.FAIL}{BColors.BOLD}Movie is not in the DB{BColors.ENDC}')
+            continue
+
+        rating = float(input(f'\nType {title} new rating: '))
+        if rating in range(1, 11):
+            storage.update_movie(title, rating)
+            print(f'\n{BColors.OK_GREEN}'
+                  f'{BColors.BOLD}{title} rating changed to {rating}{BColors.ENDC}')
+            break
+
+        print(f'\n{BColors.FAIL}{BColors.BOLD}Rating out of range!{BColors.ENDC}')
+        continue
+
+def command_get_stats():
+    """Shows general statistics about the database."""
+    movies = storage.list_movies()
+    best_movies = []
+    worst_movies = []
+    all_ratings = []
+
+    for title, info in movies.items():
+        all_ratings.append(info['rating'])
+        if info['rating'] >= max(all_ratings):
+            best_movies.append(title)
+        elif info['rating'] <= min(all_ratings):
+            worst_movies.append(title)
+
+    avg_rating = round(sum(all_ratings) / len(movies), 1)
+    median_rating = round(statistics.median(all_ratings), 1)
+
+    print(f'\n{BColors.OK_CYAN}Avg rating: {avg_rating}{BColors.ENDC}')
+    print(f'{BColors.OK_BLUE}Median rating: {median_rating}{BColors.ENDC}')
+
+    for best_movie in best_movies:
+        movie_rating = movies[best_movie]["rating"]
+        if movie_rating == max(all_ratings):
+            print(f'{BColors.OK_GREEN}'
+                  f'Best movie: {best_movie}, {movie_rating}{BColors.ENDC}')
+
+    for worst_movie in worst_movies:
+        movie_rating = movies[worst_movie]["rating"]
+        if movie_rating == min(all_ratings):
+            print(f'{BColors.WARNING}Worst movie: {worst_movie}, '
+                  f'{movies[worst_movie]["rating"]}{BColors.ENDC}')
+
+
+def command_random_movie():
+    """Pick a random movie from the database."""
+    movies = storage.list_movies()
+    title, info = random.choice(list(movies.items()))
+    print(f"\n{BColors.WARNING}"
+          f"{BColors.BOLD}Let's watch {title}, it has a rating of {info['rating']}!{BColors.ENDC}")
+
+
+def command_search_movie():
+    """Search for a movie in the database by title."""
+    movies = storage.list_movies()
+    while True:
+        search = input(f"\n{BColors.WARNING}"
+                       f"{BColors.BOLD}Enter part of a movie title: {BColors.ENDC}")
+
+        count = 0
+        for title, info in movies.items():
+            if search.lower() in title.lower():
+                count += 1
+                print(f"{BColors.OK_GREEN}{title} -> "
+                      f"{info['rating']} ({info['year']}){BColors.ENDC}")
+
+        if count == 0:
+            print(f'\n{BColors.FAIL}{BColors.BOLD}Nothing found!{BColors.ENDC}')
+            continue
+        break
+
+
+def command_sort_movies():
+    """Sorts the movies in the database by rating DESC."""
+    movies = storage.list_movies()
+    sorted_movies = sorted(movies.items(), key=lambda item: item[1]['rating'], reverse=True)
+    print() # spacing for readability
+    for title, info in sorted_movies:
+        print(f'{BColors.OK_BLUE}{title} -> {info["rating"]} ({info["year"]}){BColors.ENDC}')
+
+
+def command_generate_website():
+    # Define paths
+    current_dir = os.getcwd()
+    index_template = "index_template.html"
+    index = "index.html"
+    style = "style.css"
+
+    # Join paths for compatibility
+    src_index_path = os.path.join(current_dir, "_static", index_template)
+    dst_index_path = os.path.join(current_dir, "static", index)
+    src_css_path = os.path.join(current_dir, "_static", style)
+    dst_css_path = os.path.join(current_dir, "static", style)
+
+    # Create static folder if it doesn't exist
+    os.makedirs(os.path.dirname(dst_index_path), exist_ok=True)
+    # Copy files from _static to static
+    shutil.copy(src_index_path, dst_index_path)
+    shutil.copy(src_css_path, dst_css_path)
+    print(f"{BColors.OK_GREEN}Website was generated successfully.{BColors.ENDC}")
+
+
+def main():
+    """Runs the CLI in loop"""
+    is_cli_open = True
+    all_options = {
+      1: command_list_movies,
+      2: command_add_movie,
+      3: command_delete_movie,
+      4: update_movie,
+      5: command_get_stats,
+      6: command_random_movie,
+      7: command_search_movie,
+      8: command_sort_movies,
+      9: command_generate_website,
+      }
+
+    while is_cli_open:
+        try:
+            selected_option = int(input(CLI_MAIN_MENU))
+
+            if selected_option in range(9):
+                if selected_option == 0:
+                    print(f"\n{BColors.WARNING}"
+                          f"{BColors.BOLD}{'〰️' * 10} Bye!! {'〰️' * 10}{BColors.ENDC}\n")
+                    break
+            all_options[selected_option]()
+
+            press_enter = input(f'\n{BColors.HEADER}'
+                                f'{BColors.BOLD}Press enter to continue{BColors.ENDC}')
+
+            if press_enter == '':
+                continue
+
+        except KeyError as key:
+            print(f'\n{BColors.FAIL}{BColors.BOLD}Option {key} not valid!{BColors.ENDC}')
+
+        except ValueError:
+            print(f'\n{BColors.FAIL}{BColors.BOLD}Type a number to choose an option!{BColors.ENDC}')
+
+
+if __name__ == "__main__":
+    main()
